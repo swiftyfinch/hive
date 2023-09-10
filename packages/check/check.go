fc468421cd8d308f1ca1e90474632942d01747fb
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-09-10 23:40:48 +0500

    Add plan for check subcommand

--- packages/check/check.go
+++ packages/check/check.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func Check() {
 	fmt.Println("Check")
+
+	// TODO:
+	// 1. Read rules
+	// 2. Validate rules
+	// 3. Read modules
+	// 4. Validate modules
+	// 5. Read local and remote pods
+	// 6. Check rules for local modules
 }
