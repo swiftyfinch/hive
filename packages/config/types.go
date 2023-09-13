0d2dbfc0565f12ecb70f5dd8df52770a8a31dda3
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-09-13 22:17:45 +0500

    Add AllModulesTypes func to config

--- packages/config/types.go
+++ packages/config/types.go
@@ -1,5 +1,7 @@
 package config
 
+import "fmt"
+
 type ModulesMap map[string]*string
 
 type Modules struct {
@@ -11,4 +13,21 @@ type Config struct {
 	Types   []string            `yaml:"types"`
 	Bans    []map[string]string `yaml:"bans"`
 	Modules Modules             `yaml:"modules"`
+}
+
+func (config Config) AllModulesTypes() (map[string]string, error) {
+	moduleTypes := map[string]string{}
+	for key, value := range config.Modules.Remote {
+		if value == nil {
+			return nil, fmt.Errorf("module '%s' has empty type", key)
+		}
+		moduleTypes[key] = *value
+	}
+	for key, value := range config.Modules.Local {
+		if value == nil {
+			return nil, fmt.Errorf("module '%s' has empty type", key)
+		}
+		moduleTypes[key] = *value
+	}
+	return moduleTypes, nil
 }
