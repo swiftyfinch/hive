405937f0a043dd14e9e73e4b51257e78d4d0fad0
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-09-13 22:21:19 +0500

    Add func to check dependencies

--- packages/check/check.go
+++ packages/check/check.go
@@ -22,37 +22,52 @@ func Check(configPath string) error {
 	}
 
 	// Merge modules from config
-	moduleTypes := map[string]string{}
-	for key, value := range config.Modules.Remote {
-		if value == nil {
-			return fmt.Errorf("module '%s' has empty type", key)
-		}
-		moduleTypes[key] = *value
-	}
-	for key, value := range config.Modules.Local {
-		if value == nil {
-			return fmt.Errorf("module '%s' has empty type", key)
-		}
-		moduleTypes[key] = *value
+	moduleTypes, err := config.AllModulesTypes()
+	if err != nil {
+		return err
 	}
 
 	// Validate
-	failures := []ValidationFailure{}
-	for _, module := range localPods {
+	failures, err := checkDependencies(localPods, config.Bans, moduleTypes)
+	if err != nil {
+		return err
+	}
+	for _, failure := range failures {
+		fmt.Println(formatMessage(failure))
+	}
+
+	return nil
+}
+
+type validationFailure struct {
+	ModuleName     string
+	ModuleType     string
+	DependencyName string
+	DependencyType string
+	IsWarning      bool
+}
+
+func checkDependencies(
+	modules map[string]cocoapods.Pod,
+	bans []map[string]string,
+	moduleTypes map[string]string,
+) ([]validationFailure, error) {
+	failures := []validationFailure{}
+	for _, module := range modules {
 		moduleType, ok := moduleTypes[module.Name]
 		if !ok {
-			return fmt.Errorf("can't find type of module '%s'", module.Name)
+			return nil, fmt.Errorf("can't find type of module '%s'", module.Name)
 		}
 		for _, dependency := range module.Dependencies {
 			dependencyType, ok := moduleTypes[dependency]
 			if !ok {
-				return fmt.Errorf("can't find type of module '%s'", dependency)
+				return nil, fmt.Errorf("can't find type of module '%s'", dependency)
 			}
-			for _, rule := range config.Bans {
+			for _, rule := range bans {
 				if banDependency, ok := rule[moduleType]; ok && banDependency == dependencyType {
 					severity, ok := rule["severity"]
 					isWarning := ok && severity == "warning"
-					failures = append(failures, ValidationFailure{
+					failures = append(failures, validationFailure{
 						module.Name, moduleType,
 						dependency, dependencyType,
 						isWarning,
@@ -61,23 +76,10 @@ func Check(configPath string) error {
 			}
 		}
 	}
-
-	for _, failure := range failures {
-		fmt.Println(formatMessage(failure))
-	}
-
-	return nil
+	return failures, nil
 }
 
-type ValidationFailure struct {
-	ModuleName     string
-	ModuleType     string
-	DependencyName string
-	DependencyType string
-	IsWarning      bool
-}
-
-func formatMessage(failure ValidationFailure) string {
+func formatMessage(failure validationFailure) string {
 	moduleName, moduleType := failure.ModuleName, failure.ModuleType
 	dependencyName, dependencyType := failure.DependencyName, failure.DependencyType
 	return fmt.Sprintf("forbidden dependency %s(%s) → %s(%s)", moduleName, moduleType, dependencyName, dependencyType)
